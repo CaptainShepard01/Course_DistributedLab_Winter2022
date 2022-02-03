e7f8a127702ab4d3e77f58d6ce3bedf2fa088c3e
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-02-03 18:16:43 +0200

    Writing necessary functions for SHA-1

--- src/main/java/Level_1/Hash/SHA_1.java
+++ src/main/java/Level_1/Hash/SHA_1.java
@@ -8,6 +8,8 @@ import static java.lang.Math.pow;
 public class SHA_1 {
     private final int BLOCK_SIZE = 512;
     private final int WORD_SIZE = 32;
+    private final long MODULO = (long) pow(2, 32);
+
 
     private List<List<Long>> divideIntoWords(String padded) {
         List<List<Long>> words = new ArrayList<>(padded.length() / BLOCK_SIZE);
@@ -46,7 +48,37 @@ public class SHA_1 {
         return padded.toString();
     }
 
+    private Long ROTL(long x, int n) {
+        return ((x << n) | (x >> (WORD_SIZE - n))) % MODULO;
+    }
 
+    private Long f_t(long x, long y, long z, int t) {
+        if (0 <= t && t < 20) {
+            return (x & y) ^ (~x & z);
+        }
+        if (20 <= t && t < 40) {
+            return x ^ y ^ z;
+        }
+        if (40 <= t && t < 60) {
+            return (x & y) ^ (x & z) ^ (y & z);
+        } else {
+            return x ^ y ^ z;
+        }
+    }
+
+    private Long K_t(int t) {
+        if (0 <= t && t < 20) {
+            return 0x5a827999L;
+        }
+        if (20 <= t && t < 40) {
+            return 0x6ed9eba1L;
+        }
+        if (40 <= t && t < 60) {
+            return 0x8f1bbcdcL;
+        } else {
+            return 0xca62c1d6L;
+        }
+    }
 
     public static void main(String[] args) {
 
